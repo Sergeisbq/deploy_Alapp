314b47eeaf22fecf934c9498706d72eafb7f8a83
Sergeisbq <ss_b@icloud.com>
2023-07-03 14:10:42 +0300

    3

--- build.sh
+++ build.sh
@@ -1,16 +1,4 @@
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
-
-# pip install -r requirements.txt
-
-# python manage.py collectstatic --no-input
-# python manage.py migrate
-
 #!/usr/bin/env bash
-
-set -o errexit
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-poetry install
-python manage.py collectstatic --no-input
+pip install -r requirements/production.txt
 python manage.py migrate
+python manage.py collectstatic
